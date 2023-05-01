0298bfb72ebdd85bff54e6c3f50d392490d1d28a
Dhruv Tyagi <dhruvtyagi01@gmail.com>
2023-05-01 19:41:51 +0530

    .

---
+++ script.js
@@ -0,0 +1,25 @@
+let API_key = "7e709654e061c0a9e96019499e8137a4";
+
+// https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+// https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+/*
+"main": {
+    "temp": 298.48,
+    "feels_like": 298.74,
+    "temp_min": 297.5weather 6,
+    "temp_max": 300.05,
+    "pressure": 1015,
+    "humidity": 64,
+    "sea_level": 1015,
+    "grnd_level": 933
+  },
+"rain": {
+    "1h": 3.16
+  },
+
+  "clouds": {
+    "all": 100
+  },
+
+
+*/
